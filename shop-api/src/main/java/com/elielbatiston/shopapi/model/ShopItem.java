4d73729f367f7cb62ccfff204e6d737b7be0d280
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-04-17 17:22:13 -0300

    feat: add Shop and ShopItem model

---
+++ shop-api/src/main/java/com/elielbatiston/shopapi/model/ShopItem.java
@@ -0,0 +1,43 @@
+package com.elielbatiston.shopapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "shop_item")
+public class ShopItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "product_identified")
+	private String productIdentifier;
+	
+	private Integer amount;
+	
+	private Float price;
+	
+	@ManyToOne
+	@JoinColumn(name = "shop_id")
+	private Shop shop;
+	
+	public static ShopItem convert(ShopItemDTO shopItemDTO) {
+		ShopItem shopItem = new ShopItem();
+		
+		shopItem.setProductIdentifier(shopItemDTO.getProductIdentifier());
+		shopItem.setAmount(shopItemDTO.getAmount());
+		shopItem.setPrice(shopItemDTO.getPrice());
+		
+		return shopItem;
+	}
+}
