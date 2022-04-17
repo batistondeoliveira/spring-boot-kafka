b592fd48d346a88febb109d64cbcac4c4cf3c2f2
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-04-17 20:18:11 -0300

    feat: add Product model

---
+++ shop-validator/src/main/java/com/elielbatiston/shopvalidator/model/Product.java
@@ -0,0 +1,25 @@
+package com.elielbatiston.shopvalidator.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "product_identifier")
+	private String identifier;
+	
+	private Integer amount;
+}
