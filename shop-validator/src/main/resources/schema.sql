0c60a2eeae6a497a6216164beb91b97acfe9d9f3
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-04-17 22:19:08 -0300

    change: delete use of schema

--- shop-validator/src/main/resources/schema.sql
+++ shop-validator/src/main/resources/schema.sql
@@ -1,6 +1,4 @@
-create schema if not exists shop;
-
-create table shop.product (
+create table product (
   id bigserial primary key auto_increment,
   product_identifier varchar(100) not null,
   amount int not null
