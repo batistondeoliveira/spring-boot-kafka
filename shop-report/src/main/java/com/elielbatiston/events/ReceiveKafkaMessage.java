bd34767ae6eb8b7811afbf1128518d3daf562fb4
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-04-19 21:36:35 -0300

    feat: add ReceiveKafkaMessage

---
+++ shop-report/src/main/java/com/elielbatiston/events/ReceiveKafkaMessage.java
@@ -0,0 +1,34 @@
+package com.elielbatiston.events;
+
+import javax.transaction.Transactional;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.elielbatiston.dto.ShopDTO;
+import com.elielbatiston.repository.ReportRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ReceiveKafkaMessage {
+
+	private static final String SHOP_TOPIC_EVENT_NAME = "SHOP_TOPIC_EVENT";
+	
+	private final ReportRepository reportRepository;
+	
+	@Transactional
+	@KafkaListener(topics = SHOP_TOPIC_EVENT_NAME, groupId = "group_report")
+	public void listenShopTopic(ShopDTO shopDTO) {
+		try {
+			log.info("Compra recebida no tópico: {}.", shopDTO.getIdentifier());
+			
+			reportRepository.incrementShopStatus(shopDTO.getStatus());			
+		} catch (Exception e) {
+			log.error("Erro no processamento da mensagem", e);
+		}
+	}
+}
