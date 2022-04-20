f664d0951b46f9a55de79b740cf690e205d9dc36
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-04-19 21:31:31 -0300

    feat: add ReportRepository

---
+++ shop-report/src/main/java/com/elielbatiston/repository/ReportRepository.java
@@ -0,0 +1,19 @@
+package com.elielbatiston.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.elielbatiston.model.ShopReport;
+
+@Repository
+public interface ReportRepository extends JpaRepository<ShopReport, Long> {
+	
+	@Modifying
+	@Query(value = "update shop_report "
+			+ "set amount = amout + 1 "
+			+ "where identifier = :shopStatus",
+			nativeQuery = true)
+	void incrementShopStatus(String shopStatus);
+}
