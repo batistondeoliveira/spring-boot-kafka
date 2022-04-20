738acbd9f6b1886edacf34c3e98f5b742c2299ef
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-04-20 19:13:31 -0300

    feat: add KafkaAdmin and sample routines into main

--- kafka-manager/src/main/java/com/elielbatiston/KafkaManagerApplication.java
+++ kafka-manager/src/main/java/com/elielbatiston/KafkaManagerApplication.java
@@ -1,10 +1,36 @@
 package com.elielbatiston;
 
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+ 
 public class KafkaManagerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		
+		Properties properties = new Properties();
 		
+		properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		
+		AdminClient adminClient = AdminClient.create(properties);
+		
+		KafkaAdmin.create("topico-1", 2, (short) 1, adminClient);
+		KafkaAdmin.create("topico-2", 2, (short) 1, adminClient);
+		
+		KafkaAdmin.list(adminClient);
+		
+		KafkaAdmin.describe("topico-1", adminClient);
+		
+		KafkaAdmin.delete("topico-1", adminClient);
+		KafkaAdmin.delete("topico-2", adminClient);
+		
+		KafkaAdmin.listCG(adminClient);
+		
+		KafkaAdmin.deleteCG("group", adminClient);
+		
+		KafkaAdmin.describeCluster(adminClient);
 	}
 
 }
