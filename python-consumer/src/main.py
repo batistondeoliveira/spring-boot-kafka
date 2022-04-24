47d35f981b52ef2ac75a9269fee11f6db6cff08b
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-04-24 12:14:34 -0300

    fix: fix python-consumer

--- python-consumer/src/main.py
+++ python-consumer/src/main.py
@@ -3,21 +3,23 @@ from consumer import Consumer
 import json
 import logging
 
+# inicializa o consumidor
 consumer = Consumer()
 consumer.start()
 
 app = Flask(__name__)
 
-@app.route('/last')
+@app.route('/messages')
 def get_messages():
     messages = consumer.get_messages()
     return json.dumps(messages)
 
+@app.route('/last')
 def get_last():
     messages = consumer.get_messages()
     if len(messages) > 0:
         return json.dumps(messages[-1])
-    else: 
+    else:
         return 'lista vazia'
 
 @app.route('/last_ten')
@@ -29,4 +31,4 @@ def get_last_ten():
         return json.dumps(messages[-len(messages):])
 
 if __name__ == '__main__':
-    app.run()
+     app.run()
