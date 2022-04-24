a24c285436f0a3079cbe9b903882a70a7d22c3e9
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2022-04-24 11:42:18 -0300

    feat: Initial commit from phyton-consumer

---
+++ python-consumer/src/main.py
@@ -0,0 +1,32 @@
+from flask import flask
+from consumer import Consumer
+import json
+import logging
+
+consumer = Consumer()
+consumer.start()
+
+app = Flask(__name__)
+
+@app.route('/last')
+def get_messages():
+    messages = consumer.get_messages()
+    return json.dumps(messages)
+
+def get_last():
+    messages = consumer.get_messages()
+    if len(messages) > 0:
+        return json.dumps(messages[-1])
+    else: 
+        return 'lista vazia'
+
+@app.route('/last_ten')
+def get_last_ten():
+    messages = consumer.get_messages()
+    if len(messages) > 9:
+        return json.dumps(messages[-10:])
+    else:
+        return json.dumps(messages[-len(messages):])
+
+if __name__ == '__main__':
+    app.run()
